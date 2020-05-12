aaa31008a7135707670c7e51b6003db3534c6ded
Josefine <josefinesvegborn@gmail.com>
2020-05-12 16:23:32 +0200

    Define API root url in frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./App.css";
 
+const API_ROOT_URL = process.env.API_ROOT_URL || "http://localhost:3002";
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <p>Image Uploading App</p>
+        <p>{`Access the API on ${API_ROOT_URL}`}</p>
       </header>
     </div>
   );
