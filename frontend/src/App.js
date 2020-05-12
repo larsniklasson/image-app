97314a091356517b9b00f7f12f4f53c4a25a919e
Josefine <josefinesvegborn@gmail.com>
2020-05-12 22:03:44 +0200

    Add example image to frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img
+          alt=""
+          src={`${API_ROOT_URL}/img/example.jpeg`}
+          className="App-image"
+        />
         <p>Image Uploading App</p>
         <p>{`Access the API on ${API_ROOT_URL}`}</p>
       </header>
