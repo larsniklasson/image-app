608bceada7ecd23e947dc1fb4be5396c7c00126c
Josefine <josefinesvegborn@gmail.com>
2020-05-12 10:52:48 +0200

    Set up mongo database for local development

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,23 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
-app.get("/", (req, res) => {
-  res.status(200).send("Hello from the server!");
-});
-
+const DB_URL = "mongodb://localhost:27017/image-app-db";
 const PORT = 3000;
 
+// Setup db connection
+mongoose.connect(
+  DB_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  function (err) {
+    if (err) {
+      console.error("Error occurred while connecting to DB!");
+    } else {
+      console.log("Database connection established successfully");
+    }
+  }
+);
+
+// Run the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}...`);
 });
