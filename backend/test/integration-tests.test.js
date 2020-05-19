ed0db0e4a671f5b01432bada27f5dab2b413f80d
Josefine <josefinesvegborn@gmail.com>
2020-05-19 19:07:15 +0200

    Add integration test for POST /images endpoint

--- backend/test/integration-tests.test.js
+++ backend/test/integration-tests.test.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
@@ -25,5 +26,30 @@ test("Return 404 for invalid endpoint", async (done) => {
   const response = await request.get("/invalid");
   expect(response.status).toBe(404);
   expect(response.body.message).toBe(`Cannot find /invalid on this server!`);
+  done();
+});
+
+test("POST /images - upload image", async (done) => {
+  const expectedFilePath = "/img/Test.jpeg";
+
+  const response = await request
+    .post("/images")
+    .field("name", "Test")
+    .attach("photo", "test/utils/test.jpeg");
+
+  const fileExists = fs.existsSync(`public${expectedFilePath}`);
+
+  // Clean up by removing file again
+  if (fileExists) {
+    fs.unlink(`public${expectedFilePath}`, (err) => {
+      if (err) throw err;
+    });
+  }
+
+  expect(response.status).toBe(201);
+  expect(response.body.data.data.name).toBe("Test");
+  expect(response.body.data.data.path).toBe(expectedFilePath);
+  expect(fileExists).toBe(true);
+
   done();
 });
