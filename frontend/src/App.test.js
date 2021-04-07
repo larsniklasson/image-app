d3ad636536b73c5964b1ab5f610aa3d7414ff7f6
Josefine Svegborn <josefinesvegborn@gmail.com>
2021-04-07 15:48:29 +0200

    Update libs frontend

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const title = getByText(/Image Uploading App/i);
+  expect(title).toBeInTheDocument();
 });
